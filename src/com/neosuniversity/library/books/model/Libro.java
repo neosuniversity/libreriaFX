27a90c36ed84293dee06d81b32cf0f2d60dc48b6
neossoftware <mario.hidalgo@yahoo.com.mx>
2018-11-07 16:30:55 -0600

    model

---
+++ src/com/neosuniversity/library/books/model/Libro.java
@@ -0,0 +1,42 @@
+package com.neosuniversity.library.books.model;
+
+
+public class Libro {
+
+    private int id;
+    private String nombreLibro;
+    private String autor;
+    private String editorial;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreLibro() {
+        return nombreLibro;
+    }
+
+    public void setNombreLibro(String nombreLibro) {
+        this.nombreLibro = nombreLibro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+}
