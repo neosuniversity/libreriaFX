ee1b31f2a626367d2f136a5718aa0a4d7633dd6a
hhugohm <hhugohm@gmail.com>
2018-11-24 21:37:46 -0600

    update sql script for library database

--- src/com/neosuniversity/library/books/dao/sql_script.sql
+++ src/com/neosuniversity/library/books/dao/sql_script.sql
@@ -1,18 +1,27 @@
-select * from libros;
-insert into libros ( nombrelibro, autor, editorial) values ("JAVA EN 21 DIAS","DEITEL","OMEGA") ;
-insert into libros ( nombrelibro, autor, editorial) values ("MONGODB","KARLA","WESLEY") ;
-select   id_libro, nombrelibro, autor, editorial from libros;
-
-CREATE SCHEMA library;
-
-create table libros
-(
-  id_libro    int auto_increment
-    primary key,
-  nombrelibro varchar(50) null,
-  autor       varchar(50) null,
-  editorial   varchar(50) null
-);
+-- --------------------------------------------------------
+-- Autor:                        NEOSUNIVERSITY
+-- BD LIBRARY FOR FX PROJECT:    version 1.0
+-- --------------------------------------------------------
 
 
-INSERT INTO libros ( nombrelibro, autor, editorial) values (?, ? , ?) ;
+-- estructura de base de datos para library
+DROP DATABASE IF EXISTS `library`;
+CREATE DATABASE IF NOT EXISTS `library`;
+USE `library`;
+
+--  estructura para tabla library.libros
+DROP TABLE IF EXISTS `libros`;
+CREATE TABLE IF NOT EXISTS `libros` (
+  `id_libro`    int(11)      NOT NULL,
+  `nombrelibro` varchar(50)  NULL,
+  `autor`       varchar(50)  NULL,
+  `editorial`   varchar(50)  NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `libros` ADD PRIMARY KEY (`id_libro`);
+ALTER TABLE `libros`CHANGE COLUMN `id_libro` `id_libro` INT(11) NOT NULL AUTO_INCREMENT FIRST;
+
+INSERT INTO `libros` (`nombrelibro`, `autor`, `editorial`) VALUES ('MONGO DB IN ACTION', 'KYLE BANKER', 'MANNING');
+INSERT INTO `libros` (`nombrelibro`, `autor`, `editorial`) VALUES ('THE QUICK PHYTHON COOK', 'NAOMI CEDER', 'MANNING');
+INSERT INTO `libros` (`nombrelibro`, `autor`, `editorial`) VALUES ('SPRING MICROSERVICES IN ACTION', 'JHON CARNELL', 'MANNING');
+
