7117292c972c9165a089e48941b90516390efbd2
hhugohm <hhugohm@gmail.com>
2018-12-03 23:10:24 -0600

    add maven configuration

--- src/main/java/com/neosuniversity/library/books/view/Main.java
+++ src/main/java/com/neosuniversity/library/books/view/Main.java
@@ -10,7 +10,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/com/neosuniversity/library/books/view/altaLibro.fxml"));
+		 String fxmlFile = "/fxml/altaLibro.fxml";
+        //log.debug("Loading FXML for main view from: {}", fxmlFile);
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+		
+       // Parent root = FXMLLoader.load(getClass().getResource("/com/neosuniversity/library/books/view/altaLibro.fxml"));
         primaryStage.setTitle("Nuevo Libro");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
